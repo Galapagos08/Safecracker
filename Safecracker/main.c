e72f30bf48f529b277d88c5e6d32a6950aeb4de7
Dan Esrey <danesrey@dans-macbook-air.local>
2016-08-10 14:43:06 -0400

    Initial Commit

---
+++ Safecracker/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Safecracker
+//
+//  Created by Dan Esrey on 2016/10/08.
+//  Copyright © 2016 Dan Esrey. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
